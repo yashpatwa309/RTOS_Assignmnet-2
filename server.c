3179727cf951f66283558b798f8b7d806a58764f
yashpatwa309 <76003452+yashpatwa309@users.noreply.github.com>
2021-02-14 23:00:47 +0530

    Update server.c

--- server.c
+++ server.c
@@ -1,132 +1,176 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <pthread.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-// struct for requests to join group.
-// used by client to tell the server which group it wants to join.
+#define NO_GROUPS_MAX 5
+#define BUFFER_SIZE 1024
+#define GROUP_NAME_MAX 20
+#define NO_CLIENTS_MAX 100
+
+int fd;    // socket descripter
+int conn;  // connection descripter
+int connections[NO_GROUPS_MAX][NO_CLIENTS_MAX];
+char group_names[NO_GROUPS_MAX][GROUP_NAME_MAX];
+int connections_Count[NO_GROUPS_MAX];
+int group_count = 0;
+
+pthread_mutex_t newConnectionLock = PTHREAD_MUTEX_INITIALIZER;
 
 struct JoinRequest {
     char groupName[20];
     char name[20];
 };
-// once the server accepts the client's request, it returns with the
-// id of the client and the group id it assigned to it.
+
 struct JoinResponse {
     int id;
     int groupId;
 };
-// once groups are assigned, there is normal chat exchange happening.
-// this is done by sending the following msg sequence.
+
 struct Message {
     int id;
     int groupId;
     char name[20];
     char message[200];
 };
+void closeServer() {
+    printf(
+        "Are you sure you want to close the server ? All groups will be "
+        "deleted (Y/N) \n");
+    char response;
+    scanf("%c", &response);
+    if (response == 'Y' || response == 'y') {
+        printf("Closing Server\n");
+        for (int i = 0; i < group_count; i++) {
+            for (int j = 0; j < connections_Count[i]; j++) {
+                close(connections[i][j]);
+            }
+        }
+        close(fd);
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void handle_my(int sig) {
+    switch (sig) {
+        case SIGINT:
+            closeServer();
+            break;
+    }
+}
 
 struct sockaddr_in serv;  // socket variable
-int fd;                   // socket descripter
-int conn;                 // connection descripter
-struct Message message;
-// send function
-void *send_func(void *n) {
-    int sd = *(int *)n;
-    while (1) {
-        printf("Enter message: ");
-        fgets(message.message, sizeof(message.message), stdin);
-        write(sd, &message, sizeof(message));
-    }
 
-    return 0;
-}
-// recieve function
-void *recv_func(void *n) {
-    int rsd = *(int *)n;
+void *connection_handler(void *connec) {
+    int nsd = *(int *)connec;
+
+    struct JoinRequest request;
+    struct JoinResponse response;
+    read(nsd, &request, sizeof(request));  // reads incoming client requests.
+    char *name = request.name;
+    char *groupname = request.groupName;
+    printf("%s\n", name);
+    printf("%s\n", groupname);
+
+    int flag = 1;
+    int groupId;
+    int id;
+
+    pthread_mutex_lock(&newConnectionLock);
+    for (int i = 0; i < group_count; i++) {
+        // if same group name already exists, add the client
+        // to the same group and assign the group id and an id to the client.
+        if (!strcmp(groupname, group_names[i])) {
+            groupId = i;
+            id = connections_Count[i]++;
+            connections[groupId][id] = nsd;
+            flag = 0;
+            break;
+        }
+    }
+    // if the group name doesnt exist, create a new group with
+    // the given name.
+    if (flag) {
+        int i = group_count++;
+        strcpy(group_names[i], request.groupName);
+        groupId = i;
+        id = connections_Count[i]++;
+        connections[groupId][id] = nsd;
+    }
+    pthread_mutex_unlock(&newConnectionLock);
+    response.id = id;
+    response.groupId = groupId;
+
+    write(nsd, &response, sizeof(response));
     struct Message message;
+    while (read(nsd, &message, sizeof(message))) {
+      printf("%s\n", message.message);
+      printf("%d\n", message.id);
+        for (int i = 0; i < connections_Count[groupId]; i++) {
+            if (~connections[groupId][i] && i != message.id) {
+                write(connections[groupId][i], &message, sizeof(message));
+            }
+        }
+    }
+    connections[groupId][id] = -1;
+    return NULL;
+}
 
-    while (read(rsd, &message, sizeof(message))) {
-        printf("%s: %s \n", message.name, message.message);
+// main function:
+int main(int argc, char *argv[]) {
+    int fd, new_socket, valread;
+    struct sockaddr_in serv, client;
+
+    int addrlen = sizeof(serv);
+
+    pthread_t thread;
+    // opening socket
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    signal(SIGINT, handle_my);
+    socklen_t clientLen;
+    printf("socket created\n");
+    int port_no = atoi(argv[1]);
+    printf("%d\n", port_no);
+    serv.sin_family = AF_INET;
+    serv.sin_port =
+        htons(port_no);  // port at which server will listen for connections
+    serv.sin_addr.s_addr = INADDR_ANY;
+
+    int true = 1;
+    // setsockopt(fd,SOL_SOCKET,SO_REUSEAADR, &true, sizeof(int));
+
+    if (bind(fd, (struct sockaddr *)&serv, sizeof(serv)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
     }
 
-    return 0;
-}
-// signal handler for ctrl + C
-void signal_handler(int n) {
-    char ans;
-    exit(0);
-
-    if (n == SIGINT) {
-        printf("exit Y/N\n");
-        scanf("%c", &ans);
-        if (ans == 'y' || ans == 'Y') {
-            printf("The client exited gracefully\n");
-
+    if (listen(fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    clientLen = sizeof(client);
+    while (1) {
+        printf("entered loop\n");
+        conn = accept(fd, (struct sockaddr *)&serv, (socklen_t *)&addrlen);
+        printf("%d\n", conn);
+        if (pthread_create(&thread, NULL, connection_handler, (void *)&conn) <
+            0) {
+            perror("thread not created");
             exit(0);
         }
     }
-}
-// main function.
-int main(int argc, char const *argv[]) {
-    signal(SIGINT, signal_handler);  // to catch SIGINT and exit gracefully.
-    pthread_t thread;
-    // take from input argv
-    const char *clientname = argv[4];  // name of the client
-    printf("%s\n", clientname);
-    const char *clientgroup =
-        argv[5];  // group which the clien wants to add to.
-    printf("%s\n", clientgroup);
-    struct JoinRequest request;
-    strcpy(request.name, clientname);
-    strcpy(request.groupName, clientgroup);
-
-    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-    printf("socket created\n");
-    // fd = socket(AF_INET, SOCK_STREAM, 0);
-    serv.sin_family = AF_INET;
-    int port_no = atoi(argv[3]);
-    printf("%d\n", port_no);
-    serv.sin_port = htons(port_no);
-    printf("%s\n", argv[2]);
-    if (inet_pton(AF_INET, argv[2], &serv.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-    // inet_pton(AF_INET, argv[2], &serv.sin_addr); //binding client to local
-    // host connect(fd, (struct sockaddr *)&serv, sizeof(serv));
-    if (connect(fd, (struct sockaddr *)&serv, sizeof(serv)) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-
-    write(fd, &request, sizeof(request));
-    struct JoinResponse response;
-    read(fd, &response, sizeof(response));
-
-    int connectionId = response.id;
-    printf("connectionId = %d\n", connectionId);
-    int group_Id = response.groupId;
-    printf("%d %d\n", connectionId, group_Id);
-    if (pthread_create(&thread, NULL, recv_func, (void *)&fd) < 0) {
-        perror("pthread_create()");
-        exit(EXIT_FAILURE);
-    }
-    message.id = connectionId;
-    message.groupId = group_Id;
-    strcpy(message.name, clientname);
-
-    pthread_create(&thread, NULL, send_func, (void *)&fd);
-    pthread_join(thread, NULL);
-
+    pthread_exit(NULL);
+    close(fd);
     return 0;
 }
